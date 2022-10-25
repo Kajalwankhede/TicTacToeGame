b3787477f91e79e552c6172a674b0c2808a52942
Kajakwankhede <Kajalwankhede@example.com>
2022-10-25 20:47:59 +0530

    Added My TicTacToe Game

---
+++ src/com/tictactoe/TicTacToe.java
@@ -0,0 +1,20 @@
+package com.tictactoe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+
+        char[][] gameBoard = {{'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}, {' ', '|', ' ', '|', ' '}};
+        dispayBoard(gameBoard);
+    }
+
+    public static void dispayBoard(char[][] gameBoard) {  ///Display the game board
+        System.out.println("\n*** WELCOME TO TIC TAC TOE GAME ***");
+        for (int row = 0; row < gameBoard.length; row++) {  /// for the rows
+            for (int col = 0; col < gameBoard[0].length; col++) {// for column
+                System.out.print(gameBoard[row][col]);
+            }
+            System.out.println();
+        }
+    }
+}
+
--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -3,36 +3,27 @@ package com.tictactoe;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Uc12WinGAme {
+public class TicTacToeGame {
     static int playerScore = 0;
     static int computerScore = 0;
     static Scanner input = new Scanner(System.in);
 
-    /*
-        Notes:
-            _ | _ | _
-            _ | _ | _
-              |   |
-             Helpful indices
-             [0][0] , [0][2] , [0][4]
-             [1][0] , [1][2] , [1][4]
-             [2][0] , [2][2] , [2][4]
-            Player = 1
-            Computer = 2
-     */
-    public static void main(String[] args) {
 
-        char[][] gameBoard = {{'_', '|', '_', '|', '_'}, {'_', '|', '_', '|', '_'}, {' ', '|', ' ', '|', ' '}};
+            // [0][0] , [0][2] , [0][4]
+             //[1][0] , [1][2] , [1][4]
+             //[2][0] , [2][2] , [2][4]
+            //Player = 1
+           // Computer = 2
+    public static void main(String [] args){
 
-        System.out.println("\n*** WELCOME TO TIC TAC TOE GAME ***");
-
-        showBoard(gameBoard);
+        char [][] gameBoard = {{'_','|','_','|','_'},{'_', '|', '_','|','_'},{' ','|',' ','|',' '}};
+        dispayBoard(gameBoard);
         boolean gameOver = false;
         boolean playAgain = true;
 
-        while (playAgain) {
+        while(playAgain) { //ask to play again once win or tie player got.
             while (!gameOver) {
-
+                System.out.println("Welcome to Tic Tac Toe!!");
                 playerMove(gameBoard);
                 gameOver = isGameOver(gameBoard);
                 if (gameOver) {
@@ -45,19 +36,20 @@ public class Uc12WinGAme {
                     break;
                 }
             }
-            System.out.println("Player Score: " + playerScore);
-            System.out.println("Computer Score: " + computerScore);
+            System.out.println("Player Score: " +playerScore);
+            System.out.println("Computer Score: "+ computerScore);
             System.out.println("Would you like to play again? Y/N");
             input.nextLine();
             String result = input.nextLine();
 
-            switch (result) {
+            switch (result){
                 case "Y":
                 case "y":
                     playAgain = true;
                     System.out.println("Dope! Let's play again");
+                    resetBoard(gameBoard);
                     gameOver = false;
-                    showBoard(gameBoard);
+                    dispayBoard(gameBoard);
                     break;
 
                 case "N":
@@ -73,68 +65,64 @@ public class Uc12WinGAme {
     }
 
 
-    public static void showBoard (char[][] gameBoard){
-
-
-        System.out.println();
-
-
-        for (int row=0;row< gameBoard.length;row++) {
-            for (int col=0; col<gameBoard[0].length;col++) {
-
+    public static void dispayBoard(char [][] gameBoard){  ///Display the game board
+        System.out.println("\n*** WELCOME TO TIC TAC TOE GAME ***");
+        for (int row=0;row< gameBoard.length;row++) {  /// for the rows
+            for (int col=0; col<gameBoard[0].length;col++) {// for column
                 System.out.print(gameBoard[row][col]);
             }
             System.out.println();
         }
     }
 
-    public static void updateBoard(int position, int player, char[][] gameBoard) {
+
+    public static void updateBoard( int position, int player, char [][] gameBoard){
 
         char character;
 
-        if (player == 1) {
-            character = 'X';
-        } else {
-            character = 'O';
+        if(player==1){
+            character = 'X';   // player =1 having X character
+        }else{
+            character = 'O';  // Computer =2 having O character
         }
 
-        switch (position) {
+        switch (position){
 
             case 1:
-                gameBoard[0][0] = character;
-                showBoard(gameBoard);
+                gameBoard[0][0] = character; //  if the player or computer chose first position.
+                dispayBoard(gameBoard);
                 break;
             case 2:
                 gameBoard[0][2] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             case 3:
                 gameBoard[0][4] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             case 4:
                 gameBoard[1][0] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             case 5:
                 gameBoard[1][2] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             case 6:
                 gameBoard[1][4] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             case 7:
                 gameBoard[2][0] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             case 8:
                 gameBoard[2][2] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             case 9:
                 gameBoard[2][4] = character;
-                showBoard(gameBoard);
+                dispayBoard(gameBoard);
                 break;
             default:
                 break;
@@ -144,82 +132,81 @@ public class Uc12WinGAme {
     }
 
 
-    public static void playerMove(char[][] gameBoard) {
+    public static void playerMove(char[][] gameBoard){  //Ask player Position where whould like to move
         System.out.println("Please make a move. (1-9)");
-        System.out.println("Player Turn is:");
         int move = input.nextInt();
 
-        boolean result = isValidMove(move, gameBoard);
+        boolean result = isValidMove(move,gameBoard);
 
-        while (!result) {
+        while(!result){
             System.out.println("Sorry! Invalid Move. Try again");
             move = input.nextInt();
-            result = isValidMove(move, gameBoard);
+            result = isValidMove(move,gameBoard);
         }
 
         System.out.println("Player moved at position " + move);
-        updateBoard(move, 1, gameBoard);
+        updateBoard(move,1,gameBoard);
 
 
     }
 
 
-    public static boolean isValidMove(int move, char[][] gameboard) {
+    public static boolean isValidMove(int move, char[][] gameboard){ // Takes Move input from player
 
-        switch (move) {
+        switch (move){
             case 1:
-                if (gameboard[0][0] == '_') {
+                if(gameboard[0][0] == '_'){  // if position empty then return true
                     return true;
-                } else {
+                } else{
                     return false;
                 }
             case 2:
-                if (gameboard[0][2] == '_') {
+                if(gameboard[0][2] == '_'){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
             case 3:
-                if (gameboard[0][4] == '_') {
+                if(gameboard[0][4] == '_'){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
 
             case 4:
-                if (gameboard[1][0] == '_') {
+                if(gameboard[1][0] == '_'){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
             case 5:
-                if (gameboard[1][2] == '_') {
+                if(gameboard[1][2] == '_'){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
             case 6:
-                if (gameboard[1][4] == '_') {
+                if(gameboard[1][4] == '_'){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
             case 7:
-                if (gameboard[2][0] == ' ') {
+                if(gameboard[2][0] == ' '){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
             case 8:
-                if (gameboard[2][2] == ' ') {
+                if(gameboard[2][2] == ' '){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
             case 9:
-                if (gameboard[2][4] == ' ') {
+                if(gameboard[2][4] == ' '){
                     return true;
-                } else {
+                } else{
                     return false;
                 }
 
@@ -229,53 +216,52 @@ public class Uc12WinGAme {
 
     }
 
-    public static void computerMove(char[][] gameBoard) {
-        System.out.printf("Computer Turn.");
+    public static void computerMove(char [][] gameBoard){// For Playing computer
 
-        Random rand = new Random();
-        int move = rand.nextInt(9) + 1;
+        Random rand = new Random();  // used random function to play randomly by computer
+        int move = rand.nextInt(9)+1;
 
-        boolean result = isValidMove(move, gameBoard);
+        boolean result = isValidMove(move,gameBoard);
 
-        while (!result) {
-            move = rand.nextInt(9) + 1;
+        while(!result){
+            move = rand.nextInt(9)+1;
             result = isValidMove(move, gameBoard);
         }
 
-        System.out.println("Computer moved at position " + move);
-        updateBoard(move, 2, gameBoard);
+        System.out.println("Computer moved at position "+ move);
+        updateBoard(move,2,gameBoard);
     }
 
 
-    public static boolean isGameOver(char[][] gameboard) {
+    public static boolean isGameOver(char [][] gameboard){// Checking if win condion has met is to check all spaces in combination are samw
 
         //Horizontal Win
-        if (gameboard[0][0] == 'X' && gameboard[0][2] == 'X' && gameboard[0][4] == 'X') {
+        if(gameboard[0][0] == 'X'&& gameboard[0][2] == 'X' && gameboard [0][4] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[0][0] == 'O' && gameboard[0][2] == 'O' && gameboard[0][4] == 'O') {
+        if(gameboard[0][0] == 'O'&& gameboard[0][2] == 'O' && gameboard [0][4] == 'O' ){
             System.out.println("Computer Wins");
             computerScore++;
             return true;
         }
-        if (gameboard[1][0] == 'X' && gameboard[1][2] == 'X' && gameboard[1][4] == 'X') {
+        if(gameboard[1][0] == 'X'&& gameboard[1][2] == 'X' && gameboard [1][4] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[1][0] == 'O' && gameboard[1][2] == 'O' && gameboard[1][4] == 'O') {
+        if(gameboard[1][0] == 'O'&& gameboard[1][2] == 'O' && gameboard [1][4] == 'O' ){
             System.out.println("Computer Wins");
             computerScore++;
             return true;
         }
-        if (gameboard[2][0] == 'X' && gameboard[2][2] == 'X' && gameboard[2][4] == 'X') {
+        if(gameboard[2][0] == 'X'&& gameboard[2][2] == 'X' && gameboard [2][4] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[2][0] == 'O' && gameboard[2][2] == 'O' && gameboard[2][4] == 'O') {
+        if(gameboard[2][0] == 'O'&& gameboard[2][2] == 'O' && gameboard [2][4] == 'O' ) {
             System.out.println("Computer Wins");
             computerScore++;
             return true;
@@ -283,61 +269,62 @@ public class Uc12WinGAme {
 
         //Vertical Wins
 
-        if (gameboard[0][0] == 'X' && gameboard[1][0] == 'X' && gameboard[2][0] == 'X') {
+        if(gameboard[0][0] == 'X'&& gameboard[1][0] == 'X' && gameboard [2][0] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[0][0] == 'O' && gameboard[1][0] == 'O' && gameboard[2][0] == 'O') {
+        if(gameboard[0][0] == 'O'&& gameboard[1][0] == 'O' && gameboard [2][0] == 'O' ){
             System.out.println("Computer Wins");
             computerScore++;
             return true;
         }
 
-        if (gameboard[0][2] == 'X' && gameboard[1][2] == 'X' && gameboard[2][2] == 'X') {
+        if(gameboard[0][2] == 'X'&& gameboard[1][2] == 'X' && gameboard [2][2] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[0][2] == 'O' && gameboard[1][2] == 'O' && gameboard[2][2] == 'O') {
+        if(gameboard[0][2] == 'O'&& gameboard[1][2] == 'O' && gameboard [2][2] == 'O' ){
             System.out.println("Computer Wins");
             computerScore++;
             return true;
         }
 
-        if (gameboard[0][4] == 'X' && gameboard[1][4] == 'X' && gameboard[2][4] == 'X') {
+        if(gameboard[0][4] == 'X'&& gameboard[1][4] == 'X' && gameboard [2][4] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[0][4] == 'O' && gameboard[1][4] == 'O' && gameboard[2][4] == 'O') {
+        if(gameboard[0][4] == 'O'&& gameboard[1][4] == 'O' && gameboard [2][4] == 'O' ){
             System.out.println("Computer Wins");
             computerScore++;
             return true;
         }
 
         //Diagonal Wins
-        if (gameboard[0][0] == 'X' && gameboard[1][2] == 'X' && gameboard[2][4] == 'X') {
+        if(gameboard[0][0] == 'X'&& gameboard[1][2] == 'X' && gameboard [2][4] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[0][0] == 'O' && gameboard[1][2] == 'O' && gameboard[2][4] == 'O') {
+        if(gameboard[0][0] == 'O'&& gameboard[1][2] == 'O' && gameboard [2][4] == 'O' ){
             System.out.println("Computer Wins");
             computerScore++;
             return true;
         }
 
-        if (gameboard[2][0] == 'X' && gameboard[1][2] == 'X' && gameboard[0][4] == 'X') {
+        if(gameboard[2][0] == 'X'&& gameboard[1][2] == 'X' && gameboard [0][4] == 'X' ){
             System.out.println("Player Wins");
             playerScore++;
             return true;
         }
-        if (gameboard[2][0] == 'O' && gameboard[1][2] == 'O' && gameboard[0][4] == 'O') {
+        if(gameboard[2][0] == 'O'&& gameboard[1][2] == 'O' && gameboard [0][4] == 'O' ){
             System.out.println("Computer Wins");
             computerScore++;
             return true;
         }
+
 
         if (gameboard[0][0] != '_' && gameboard[0][2] != '_' && gameboard[0][4] != '_' && gameboard[1][0] != '_' &&
                 gameboard[1][2] != '_' && gameboard[1][4] != '_' && gameboard[2][0] != ' ' && gameboard[2][2] != ' ' && gameboard[2][4] != ' ') {
@@ -349,5 +336,17 @@ public class Uc12WinGAme {
         return false;
     }
 
+    public static void resetBoard(char [][] gameBoard){ // set the board back to beginning state.
+        gameBoard[0][0] = '_';
+        gameBoard[0][2] = '_';
+        gameBoard[0][4] = '_';
+        gameBoard[1][0] = '_';
+        gameBoard[1][2] = '_';
+        gameBoard[1][4] = '_';
+        gameBoard[2][0] = ' ';
+        gameBoard[2][2] = ' ';
+        gameBoard[2][4] = ' ';
 
+    }
 }
+
